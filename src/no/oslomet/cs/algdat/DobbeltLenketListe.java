467f6c367f2d3efb7e9bddeef3b6795294e55477
suhail <suhail_0310@hotmail.com>
2020-10-08 18:24:37 +0200

    Fullført kildekode for konstruktør, mangler å fikse kildekode for nodene

---
+++ src/no/oslomet/cs/algdat/DobbeltLenketListe.java
@@ -0,0 +1,195 @@
+package no.oslomet.cs.algdat;
+
+
+////////////////// class DobbeltLenketListe //////////////////////////////
+
+
+import java.util.Comparator;
+import java.util.ConcurrentModificationException;
+import java.util.NoSuchElementException;
+import java.util.StringJoiner;
+
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+
+
+public class DobbeltLenketListe<T> implements Liste<T> {
+    public static void main(String[] args){
+
+        Liste<String> liste = new DobbeltLenketListe<>();
+        System.out.println(liste.antall() + " " + liste.tom());
+    }
+
+    /**
+     * Node class
+     * @param <T>
+     */
+    private static final class Node<T> {
+        private T verdi;                   // nodens verdi
+        private Node<T> forrige, neste;    // pekere
+
+        private Node(T verdi, Node<T> forrige, Node<T> neste) {
+            this.verdi = verdi;
+            this.forrige = forrige;
+            this.neste = neste;
+        }
+
+        private Node(T verdi) {
+            this(verdi, null, null);
+        }
+    }
+
+    // instansvariabler
+    private Node<T> hode;          // peker til den første i listen
+    private Node<T> hale;          // peker til den siste i listen
+    private int antall;            // antall noder i listen
+    private int endringer;         // antall endringer i listen
+
+    public DobbeltLenketListe() {
+        hode = null;
+        hale = null;
+        endringer = 0;
+        antall = 0;
+
+    }
+
+    public DobbeltLenketListe(T[] a) {
+        //Lage en dobbeltlenketliste med verdiene fra tabellen a
+        /*
+            Krav-> Tabell kan ikke:
+                være tom-> kaste exception
+                if(a == 0)-> Sjekk om tabellen er null, hvis ikke -> finn første ikke-null-verdi
+                else->hvis kun nullverdier-> returnerer tom tabell
+                    //
+            tom-> kaste
+         */
+
+        /*
+            Sette hode først (previous må være null) og hale sist (neste må være null) VIKTIG
+            
+
+         */
+
+
+    }
+
+    public Liste<T> subliste(int fra, int til){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int antall() {
+        return antall; //returner antall i listen
+    }
+
+    @Override
+    public boolean tom() {
+        if(antall == 0){ //tom tabell
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean leggInn(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void leggInn(int indeks, T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean inneholder(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T hent(int indeks) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indeksTil(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T oppdater(int indeks, T nyverdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean fjern(T verdi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T fjern(int indeks) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void nullstill() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String omvendtString() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Iterator<T> iterator(int indeks) {
+        throw new UnsupportedOperationException();
+    }
+
+    private class DobbeltLenketListeIterator implements Iterator<T>
+    {
+        private Node<T> denne;
+        private boolean fjernOK;
+        private int iteratorendringer;
+
+        private DobbeltLenketListeIterator(){
+            denne = hode;     // p starter på den første i listen
+            fjernOK = false;  // blir sann når next() kalles
+            iteratorendringer = endringer;  // teller endringer
+        }
+
+        private DobbeltLenketListeIterator(int indeks){
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean hasNext(){
+            return denne != null;
+        }
+
+        @Override
+        public T next(){
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void remove(){
+            throw new UnsupportedOperationException();
+        }
+
+    } // class DobbeltLenketListeIterator
+
+    public static <T> void sorter(Liste<T> liste, Comparator<? super T> c) {
+        throw new UnsupportedOperationException();
+    }
+
+} // class DobbeltLenketListe
